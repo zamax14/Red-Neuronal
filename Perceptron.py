8c423409d288dcbcb503ac7792d87174eac2b308
Alex Zarate <zarate.alejandroM14@live.com.mx>
2022-02-20 21:08:37 -0600

    Input de epocas y fix al algoritmo

--- Perceptron.py
+++ Perceptron.py
@@ -19,12 +19,11 @@ def change_color():
 
 def plot_point(event):
     ix, iy = event.xdata, event.ydata
+    X.append((ix, iy))
     if good_or_bad:
-        X.append((ix, iy))
         d.append(1)
         ax.plot(ix,iy, '.g')
     else:
-        X.append((ix, iy))
         d.append(0)
         ax.plot(ix,iy, '.r')
     canvas.draw()
@@ -32,11 +31,13 @@ def plot_point(event):
 def print_axis():
     global ax
     #Imprimimos los ejes del plano cartesiano
-    ejeX = [-20,20]
-    ejeY = [-20,20]
+    ejeX = [-2,2]
+    ejeY = [-2,2]
     zeros = [0,0]
     ax.plot(ejeX, zeros, 'k')
     ax.plot(zeros, ejeY, 'k')
+    plt.xlim(-2,2)
+    plt.ylim(-2,2)
 
 def ActivationFunc():
     global w1, w2, theta
@@ -46,21 +47,23 @@ def ActivationFunc():
     return F_u
 
 def print_line():
-    global w1, w2, theta, eta, X
+    global w1, w2, theta, eta, epoch_inter, X, d
 
-    epoch = 250
+    epoch = int(epoch_inter.get())
     error = True
-    while (epoch!=0) and error:
-        e = []
+
+    while epoch and error:
+        error = False
         for i in range(len(X)):
             Y = np.dot(X[i],[w1,w2])-theta >= 0
-            e.append(d[i]-Y)
-            w1 = w1 + (float(eta.get())*e[-1]*X[i][0])
-            w2 = w2 + (float(eta.get())*e[-1]*X[i][1])
-            theta = theta + (float(eta.get())*e[-1]*1)
+            e = d[i]-Y
+            if e != 0:
+                error = True
+                w1 = w1 + (float(eta.get())*e*X[i][0])
+                w2 = w2 + (float(eta.get())*e*X[i][1])
+                theta = theta - (float(eta.get())*e)
 
         ax.cla()
-        print_axis()
 
         Y=[]
         m=-w1/w2
@@ -70,36 +73,37 @@ def print_line():
         for i in range(len(X)):
             #Si la funcion f(u) da 1, entonces el punto se imprime de color verde
             #En caso contrario será rojo
-            if d[i]:
+            if Y[i]:
                 ax.plot(X[i][0],X[i][1],'.g')
             else:
                 ax.plot(X[i][0],X[i][1],'.r')
 
         #Coloca una linea a partir de un punto dado y la pendiente
-        print(X[0][0], m, b)
         plt.axline((X[0][0], (X[0][0]*m)+b), slope=m, color='b')
-        canvas.draw()
+        print_axis()
+
         W1_label.config(text="W1: {:.4f}".format(w1))
         W2_label.config(text="W2: {:.4f}".format(w2))
         Theta_label.config(text="Theta: {:.4f}".format(theta))
-
-        if not(1 in e) and not(-1 in e):
-            error = False
-            
+          
         epoch-=1
 
+        canvas.draw()
+
 def clean_screen():
-    global X
+    global X, d
+    X = []
+    d = []
     ax.cla()
     print_axis()
     canvas.draw()
-    X = []
     
 
 #Inizializamos la grafica de matplotlib
 fig, ax= plt.subplots(facecolor='#8D96DA')
 fig.canvas.mpl_connect('button_press_event', plot_point)
-
+plt.xlim(-2,2)
+plt.ylim(-2,2)
 print_axis()
 
 mainwindow = Tk()
@@ -110,6 +114,7 @@ w1 = random.random()
 w2 = random.random()
 theta = random.random()
 eta = StringVar(mainwindow, 0)
+epoch_inter = StringVar(mainwindow, 0)
 #Colocamos la grafica en la interfaz
 canvas = FigureCanvasTkAgg(fig, master = mainwindow)
 canvas.get_tk_widget().place(x=10, y=10, width=580, height=580)
@@ -130,15 +135,20 @@ Eta_label.place(x=600, y=110)
 Eta_entry = Entry(mainwindow, textvariable=eta)
 Eta_entry.place(x=600, y=130) 
 
-#button
+Epoch_label = Label(mainwindow, text = "Num. Epoch: ")
+Epoch_label.place(x=600, y=160)
+
+Epoch_entry = Entry(mainwindow, textvariable=epoch_inter)
+Epoch_entry.place(x=600, y=180)
+
 start_button = Button(mainwindow, text="Go!", command=lambda:threading.Thread(target=print_line).start())
 start_button.place(x=600, y=230)
 
 start_button = Button(mainwindow, text="Clean", command=clean_screen)
-start_button.place(x=600, y=270)
+start_button.place(x=600, y=260)
 
 red_green_button = Button(mainwindow, text="Good", background="#2CA711", width=10, command=change_color)
-red_green_button.place(x=600, y=300)
+red_green_button.place(x=600, y=290)
 
 #Mostramos la interfaz
 mainwindow.mainloop()
