994f1a550d5396f2e3a6dfd91163e60cffa46955
Alex Zarate <zarate.alejandroM14@live.com.mx>
2022-03-01 21:55:03 -0600

    Sigmoidal Function

--- Perceptron.py
+++ Perceptron.py
@@ -19,6 +19,7 @@ def change_color():
 
 def plot_point(event):
     ix, iy = event.xdata, event.ydata
+    print("{:.2f}".format(ix), "{:.2f}".format(iy))
     X.append((ix, iy))
     if good_or_bad:
         d.append(1)
@@ -41,27 +42,30 @@ def print_axis():
 
 def ActivationFunc():
     global w1, w2, theta
-    #Generamos el vector F(u) con true y false
-    F_u = np.dot(X,[w1,w2])-theta >= 0
-    #Retornamos f(u), los valores de X, m y b
+
+    v = np.dot(X,[w1,w2])-theta
+    F_u =  1/(1+np.exp(-(float(a.get()))*v))
+
     return F_u
 
 def print_line():
-    global w1, w2, theta, eta, epoch_inter, X, d
+    global w1, w2, theta, eta, epoch_inter, X, d, a
 
     epoch = 1
-    error = True
-
-    while epoch<=int(epoch_inter.get()) and error:
-        error = False
+    e = [1]
+    
+    while np.average(np.power(e,2)) > 0.001:
+        e = []
         for i in range(len(X)):
-            Y = np.dot(X[i],[w1,w2])-theta >= 0
-            e = d[i]-Y
-            if e != 0:
-                error = True
-                w1 = w1 + (float(eta.get())*e*X[i][0])
-                w2 = w2 + (float(eta.get())*e*X[i][1])
-                theta = theta - (float(eta.get())*e)
+            v = np.dot(X[i],[w1,w2])-theta
+            Y = 1/(1+np.exp(-(float(a.get()))*v))
+            e.append(d[i]-Y)
+            y_prima = float(a.get())*Y*(1-Y)
+            w1 = w1 + (float(eta.get())*e[-1]*y_prima*X[i][0])
+            w2 = w2 + (float(eta.get())*e[-1]*y_prima*X[i][1])
+            theta = theta - (float(eta.get())*e[-1]*y_prima)
+
+        print(np.average(np.power(e,2)))
 
         ax.cla()
 
@@ -71,9 +75,7 @@ def print_line():
         Y = ActivationFunc()
         #Imprimimos los puntos en la grafica
         for i in range(len(X)):
-            #Si la funcion f(u) da 1, entonces el punto se imprime de color verde
-            #En caso contrario será rojo
-            if Y[i]:
+            if Y[i]>=0.5:
                 ax.plot(X[i][0],X[i][1],'.g')
             else:
                 ax.plot(X[i][0],X[i][1],'.r')
@@ -120,6 +122,7 @@ w1 = random.random()
 w2 = random.random()
 theta = random.random()
 eta = StringVar(mainwindow, 0)
+a = StringVar(mainwindow, 0)
 epoch_inter = StringVar(mainwindow, 0)
 #Colocamos la grafica en la interfaz
 canvas = FigureCanvasTkAgg(fig, master = mainwindow)
@@ -147,14 +150,20 @@ Epoch_label.place(x=600, y=160)
 Epoch_entry = Entry(mainwindow, textvariable=epoch_inter)
 Epoch_entry.place(x=600, y=180)
 
-start_button = Button(mainwindow, text="Go!", command=lambda:threading.Thread(target=print_line).start())
-start_button.place(x=600, y=230)
+a_label = Label(mainwindow, text = "A: ")
+a_label.place(x=600, y=210)
 
-start_button = Button(mainwindow, text="Clean", command=clean_screen)
+a_entry = Entry(mainwindow, textvariable=a)
+a_entry.place(x=600, y=230)
+
+start_button = Button(mainwindow, text="Go!", command=lambda:threading.Thread(target=print_line).start())
 start_button.place(x=600, y=260)
 
+start_button = Button(mainwindow, text="Clean", command=clean_screen)
+start_button.place(x=600, y=280)
+
 red_green_button = Button(mainwindow, text="Good", background="#2CA711", width=10, command=change_color)
-red_green_button.place(x=600, y=290)
+red_green_button.place(x=600, y=300)
 
 #Mostramos la interfaz
 mainwindow.mainloop()
---
+++ test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+e = [1,2,3,4,5,6]
+print(np.average(e), (np.average(np.power(e,2))))
